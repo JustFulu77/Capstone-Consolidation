5521d7100d4b076ace004334dd57d26a101bfa7f
Fulufhelo Ganyane <fulug7@gmail.com>
2025-11-02 19:36:46 +0200

    Generate Sphinx documentation with docstrings

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import django
+
+sys.path.insert(0, os.path.abspath('../..'))  # adjust to project root
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'  # replace Newsletter with your project name
+django.setup()
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Fulu'
+copyright = '2025, Fulu'
+author = 'Fulu'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme =  'sphinx_rtd_theme'
+html_static_path = ['_static']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # for Google-style docstrings
+]
